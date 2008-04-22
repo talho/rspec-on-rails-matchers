b09228f56d8b9612511d471ecf38c837685fdacb
Brandon Keepers <brandon@collectiveidea.com>
2008-04-22 01:15:17 -0400

    DRY up the view matchers

--- lib/spec/rails/matchers/views.rb
+++ lib/spec/rails/matchers/views.rb
@@ -19,84 +19,49 @@ module Spec
           have_tag("label[for=#{attribute}]").matches?(response)
         end
       end
+      alias_method :with_label_for, :have_label_for
       
-      def with_label_for(attribute, text)
-        return simple_matcher("have a label for '#{attribute}' with value of '#{text}'") do |response|
-          with_tag("label[for=#{attribute}]").matches?(response)
-        end
-      end
-
       def have_text_field_for(attribute)
         return simple_matcher("have a text field for '#{attribute}'") do |response|
           have_tag("input##{attribute}[type=text]").matches?(response)
         end
       end
+      alias_method :with_text_field_for, :have_text_field_for
       
-      def with_text_field_for(attribute)
-        return simple_matcher("with a text field for '#{attribute}'") do |response|
-          with_tag("input##{attribute}[type=text]").matches?(response)
-        end
-      end
-
       def have_text_area_for(attribute)
         return simple_matcher("have a text field for '#{attribute}'") do |response|
           have_tag("textarea##{attribute}[type=text]").matches?(response)
         end
       end
-      
-      def with_text_area_for(attribute)
-        return simple_matcher("have a text field for '#{attribute}'") do |response|
-          with_tag("textarea##{attribute}[type=text]").matches?(response)
-        end
-      end
+      alias_method :with_text_area_for, :have_text_area_for
       
       def have_password_field_for(attribute)
         return simple_matcher("have a password field for '#{attribute}'") do |response|
           have_tag("input##{attribute}[type=password]").matches?(response)
         end
       end
+      alias_method :with_password_field_for, :have_password_field_for
 
-      def with_password_field_for(attribute)
-        return simple_matcher("have a password field for '#{attribute}'") do |response|
-          with_tag("input##{attribute}[type=password]").matches?(response)
-        end
-      end
-      
       def have_checkbox_for(attribute)
         return simple_matcher("have a checkbox for '#{attribute}'") do |response|
           have_tag("input##{attribute}[type=checkbox]").matches?(response)
         end
       end
+      alias_method :with_checkbox_for, :have_checkbox_for
       
-      def with_checkbox_for(attribute)
-        return simple_matcher("have a checkbox for '#{attribute}'") do |response|
-          with_tag("input##{attribute}[type=checkbox]").matches?(response)
-        end
-      end
-
       def have_submit_button
         return simple_matcher("have a submit button") do |response|
           have_tag("input[type=submit]").matches?(response)
         end
       end
+      alias_method :with_submit_button, :have_submit_button
       
-      def with_submit_button
-        return simple_matcher("have a submit button") do |response|
-          with_tag("input[type=submit]").matches?(response)
-        end
-      end
-
       def have_link_to(url_or_path, text = nil)
         return simple_matcher("have a link to '#{url_or_path}'") do |response|
           have_tag("a[href=#{url_or_path}]", text).matches?(response)
         end
       end
-      
-      def with_link_to(url_or_path, text = nil)
-        return simple_matcher("have a link to '#{url_or_path}'") do |response|
-          with_tag("a[href=#{url_or_path}]", text).matches?(response)
-        end
-      end
+      alias_method :with_link_to, :have_link_to
     end
   end
 end
