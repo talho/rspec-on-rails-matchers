13464a3a97675e958e7c216be2f8558aeaf60eed
Brandon Keepers <brandon@collectiveidea.com>
2008-04-22 01:03:47 -0400

    Added more robust association matchers

--- lib/spec/rails/matchers/associations.rb
+++ lib/spec/rails/matchers/associations.rb
@@ -1,33 +1,62 @@
 module Spec
   module Rails
     module Matchers
-      def belong_to(association)
-        return simple_matcher("model to belong to #{association}") do |model|
-          model = model.class if model.is_a? ActiveRecord::Base
-          model.reflect_on_all_associations(:belongs_to).find { |a| a.name == association }
+
+      class Association  #:nodoc:
+
+        def initialize(type, name, options = {})
+          @type = type
+          @name = name
+          @options = options
+          @errors = []
         end
+
+        def matches?(model)
+          @model = model.is_a?(ActiveRecord::Base) ? model.class : model
+          
+          @association = @model.reflect_on_association(@name)
+
+          unless @association
+            @errors << "#{@name} association not defined"
+          else
+            @association.check_validity!
+            unless @association.macro == @type
+              @errors << "expected #{@model} to have a #{@type.inspect} association called #{name.inspect}, but got #{@association.macro.inspect}" 
+            end
+            declared_options = @association.options.reject {|k,| !@options.keys.include?(k) }
+            unless declared_options == @options
+              @errors << "expected #{@type.inspect} #{@name.inspect} association with #{@options.inspect}, but got #{declared_options.inspect}"
+            end
+          end
+          @errors.empty?
+        end
+
+        def failure_message
+          @errors.join "\n"
+        end
+
+        def description
+          "have a #{@type} association called :#{@name}"
+        end
+
       end
 
-      def have_many(association)
-        return simple_matcher("model to have many #{association}") do |model|
-          model = model.class if model.is_a? ActiveRecord::Base
-          model.reflect_on_all_associations(:has_many).find { |a| a.name == association }
-        end
+      def have_many(name, options = {})
+        Association.new(:has_many, name, options)
+      end
+      
+      def belong_to(name, options = {})
+        Association.new(:belongs_to, name, options)
+      end
+      
+      def have_and_belong_to_many(name, options = {})
+        Association.new(:has_and_belongs_to_many, name, options)
       end
 
-      def have_one(association)
-        return simple_matcher("model to have one #{association}") do |model|
-          model = model.class if model.is_a? ActiveRecord::Base
-          model.reflect_on_all_associations(:has_one).find { |a| a.name == association }
-        end
+      def have_one(name, options = {})
+        Association.new(:has_one, name, options)
       end
 
-      def have_and_belong_to_many(association)
-        return simple_matcher("model to have and belong to many #{association}") do |model|
-          model = model.class if model.is_a? ActiveRecord::Base
-          model.reflect_on_all_associations(:has_and_belongs_to_many).find { |a| a.name == association }
-        end
-      end
     end
   end
 end
