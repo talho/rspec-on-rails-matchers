ff22443cc00ad59095cafcdfa7d27a07e2efc57e
Brandon Keepers <brandon@collectiveidea.com>
2008-04-24 15:11:34 -0400

    rewrite view matchers to be more model centric

--- lib/spec/rails/matchers/views.rb
+++ lib/spec/rails/matchers/views.rb
@@ -1,6 +1,47 @@
 module Spec
   module Rails
     module Matchers
+      class HTMLElementMatcher
+        def initialize(spec_scope, tag_name, *args)
+          @attributes = args.extract_options!
+          @tag_name = tag_name
+          @spec_scope = spec_scope
+          
+          @tag = [@tag_name, extract_id, extract_classes, build_attributes].compact.join
+          @matcher = spec_scope.send(:have_tag, @tag, *args)
+        end
+        
+        def matches?(response)
+          @matcher.matches?(response)
+        end
+        
+        [:failure_message, :negative_failure_message, :description].each do |method|
+          define_method(method) { @matcher.send(method) }
+        end
+        
+      private
+        
+        def extract_id
+          "##{@attributes.delete(:id)}" if @attributes[:id]
+        end
+
+        def extract_classes
+          classes = case css = @attributes.delete(:class)
+          when String
+            css.split(' ')
+          when Array
+            css
+          end
+          '.' + classes.join('.') if classes
+        end
+        
+        def build_attributes
+          @attributes.collect {|attr,value| "[#{attr}='#{value}']" }.join
+        end
+        
+        
+      end
+      
       def have_form_posting_to(url_or_path)
         return simple_matcher("have a form submitting via POST to '#{url_or_path}'") do |response|
           have_tag("form[method=post][action=#{url_or_path}]").matches?(response)
@@ -14,45 +55,30 @@ module Spec
         end
       end
       
-      def have_label_for(attribute, text)
-        return simple_matcher("have a label for '#{attribute}' with value of '#{text}'") do |response|
-          have_tag("label[for=#{attribute}]").matches?(response)
-        end
+      def have_label_for(model, attribute, text = nil)
+        HTMLElementMatcher.new(self, 'label', text, :for => "#{model}_#{attribute}")
       end
       alias_method :with_label_for, :have_label_for
       
-      def have_text_field_for(attribute)
-        return simple_matcher("have a text field for '#{attribute}'") do |response|
-          have_tag("input##{attribute}[type=text]").matches?(response)
-        end
-      end
-      alias_method :with_text_field_for, :have_text_field_for
-      
-      def have_text_area_for(attribute)
-        return simple_matcher("have a text field for '#{attribute}'") do |response|
-          have_tag("textarea##{attribute}[type=text]").matches?(response)
-        end
+      def have_text_area_for(model, attribute)
+        HTMLElementMatcher.new(self, 'textarea',
+          :id => "#{model}_#{attribute}",
+          :name => "#{model}[#{attribute}]")
       end
       alias_method :with_text_area_for, :have_text_area_for
       
-      def have_password_field_for(attribute)
-        return simple_matcher("have a password field for '#{attribute}'") do |response|
-          have_tag("input##{attribute}[type=password]").matches?(response)
+      %w(text password checkbox).each do |input|
+        define_method("have_#{input}_field_for") do |model, attribute|
+          HTMLElementMatcher.new(self, 'input',
+            :type => input,
+            :id => "#{model}_#{attribute}",
+            :name => "#{model}[#{attribute}]")
         end
+        alias_method :"with_#{input}_field_for", :"have_#{input}_field_for"
       end
-      alias_method :with_password_field_for, :have_password_field_for
-
-      def have_checkbox_for(attribute)
-        return simple_matcher("have a checkbox for '#{attribute}'") do |response|
-          have_tag("input##{attribute}[type=checkbox]").matches?(response)
-        end
-      end
-      alias_method :with_checkbox_for, :have_checkbox_for
       
-      def have_submit_button
-        return simple_matcher("have a submit button") do |response|
-          have_tag("input[type=submit]").matches?(response)
-        end
+      def have_submit_button(options = {})
+        HTMLElementMatcher.new(self, 'input', options.merge(:type => 'submit'))
       end
       alias_method :with_submit_button, :have_submit_button
       
